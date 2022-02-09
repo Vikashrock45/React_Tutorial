f29fb6a0eb303d56a5448fc8f1912f90ea612376
Vikash Kumar <75295300+Vikashrock45@users.noreply.github.com>
2022-02-09 12:43:37 +0530

    Changes last name

--- src/index.js
+++ src/index.js
@@ -11,17 +11,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const fname = "Vikash";
-const lname = "Kr";
+const lname = "Rock";
 const year = 2021;
 
 ReactDOM.render(
   <div>
-    <p>
-      Created by {fname + " " + lname}
-    </p>
-    <p>
-      Copyright {year}
-    </p>
+    <p>Created by {fname + " " + lname}</p>
+    <p>Copyright {year}</p>
   </div>,
   document.getElementById("root")
 );
